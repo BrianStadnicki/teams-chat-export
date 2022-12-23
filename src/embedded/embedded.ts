2f0487feff25f8120e2681d822004a5ac55fa996
Brian Stadnicki <stadnickibrian@gmail.com>
2022-12-23 19:10:18 +0000

    add checkboxes to posts

--- src/embedded/embedded.ts
+++ src/embedded/embedded.ts
@@ -15,7 +15,7 @@ function createTeamsListCheckboxes() {
     }
 
     document.querySelectorAll(".team").forEach(team => {
-        let observer = new MutationObserver((mutations, observer) => {
+        new MutationObserver((mutations, observer) => {
             mutations.forEach((mutation) => {
                 if (mutation.type === "childList") {
                     mutation.addedNodes.forEach(node => {
@@ -28,9 +28,45 @@ function createTeamsListCheckboxes() {
                     })
                 }
             })
-        });
-        observer.observe(team, {childList: true});
+        }).observe(team, {childList: true});
     });
+}
+
+function createPostsListCheckboxes() {
+    let insertCheckbox = (postDiv: HTMLDivElement) => {
+        if (postDiv.querySelector(".--embedded-chat-export-selector-post") !== null) {
+            return;
+        }
+
+        let insertPosition = postDiv.querySelector(".clearfix > thread > .ts-message > .conversation-common > thread-body > .thread-body > .media-left");
+        if (insertPosition === null) {
+            return;
+        }
+
+        let input = document.createElement("input");
+        input.type = "checkbox";
+        input.classList.add("--embedded-chat-export-selector-post");
+        insertPosition.insertAdjacentElement("beforeend", input);
+    }
+
+    let posts = document.getElementsByClassName("ts-message-list-item");
+    for (let i = 0; i < posts.length; i++) {
+        insertCheckbox(<HTMLDivElement>posts.item(i));
+    }
+
+    new MutationObserver((mutations, observer) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === "childList") {
+                mutation.addedNodes.forEach(node => {
+                    if ((<HTMLElement>node).tagName === "DIV" &&
+                        (<HTMLElement>node).classList.contains("ts-message-list-item")) {
+
+                        insertCheckbox(<HTMLDivElement>node);
+                    }
+                })
+            }
+        })
+    }).observe(document.getElementsByClassName("ts-message-list-container").item(0), {childList: true});
 }
 
 setInterval(() => {
@@ -48,5 +84,6 @@ setInterval(() => {
 
     document.getElementById("--embedded-chat-export-headbar-export-btn").onclick = () => {
         createTeamsListCheckboxes();
+        createPostsListCheckboxes();
     };
 }, 50);
