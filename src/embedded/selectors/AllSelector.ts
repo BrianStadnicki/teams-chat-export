b0f4af1b40a203091b3c9f043935b22b5f37d0ee
Brian Stadnicki <stadnickibrian@gmail.com>
2022-12-26 18:16:16 +0000

    Select all in AllSelector

--- src/embedded/selectors/AllSelector.ts
+++ src/embedded/selectors/AllSelector.ts
@@ -2,6 +2,7 @@ import {CheckboxSelector} from "./CheckboxSelector";
 
 export class AllSelector extends CheckboxSelector {
     init() {
+        this.enablePostHook = (post) => true;
         super.init();
         this.startLoading();
     }
